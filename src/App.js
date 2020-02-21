c3bae16d5e616e133569d80cf2a75cbac6236ec4
laurislopata <lopatalaurynas@gmail.com>
2020-02-21 22:15:40 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "TODO1",
+      isCompleted: false
+    },
+    {
+      text: "TODO2",
+      isCompleted: false
+    },
+    {
+      text: "TODO3",
+      isCompleted: false
+    }
+  ])
+  const completeTodo = index => {
+    const newTodos=[...todos];
+    newTodos[index].isCompleted=true;
+    setTodos(newTodos);
+  }
+  const addTodo = text => {
+    const newTodos = [...todos, { text: text, isCompleted:true }];
+    setTodos(newTodos);
+  }
+  const removeTodo = index => {
+    const newTodos=[...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        {todos.map((todo, index) => (
+          <Todo className="todo"
+            key={index}
+            todo={todo}
+            index={index}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("aaa");
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("aaa");
+  }
+  return (
+    <form onSubmit={handleSubmit} className="form">
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete</button>
+        <button onClick={() => removeTodo(index)}>Remove</button>
+      </div>
     </div>
   );
 }
